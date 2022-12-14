537dd6bb64efb467d6e0d12a25027f203238f4e3
Nicholas Brancifort <brancifortnick@yahoo.com>
2022-12-13 19:56:53 -0500

    app.js modified

--- app.js
+++ app.js
@@ -63,19 +63,19 @@ app.use('/my-trails', collectionsRouter);
 app.use('/reviews',reviewsRouter);
 
 // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   //console.log(err)
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //console.log(err)
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
